79aca9a0a37d4c26bbfe48d7cdf9cba92a4b88a4
cxdeng <chengx.deng@icloud.com>
2022-12-01 08:24:41 +0000

    Singly linked list

--- LinkedList/Header/SinglyLinkedList.h
+++ LinkedList/Header/SinglyLinkedList.h
@@ -13,3 +13,52 @@ typedef struct Node
     ElemType data;
     struct Node *next;
 } SinglyLinkedList;
+
+/**
+ * Initialize a singly linked list with a head node
+ * @return
+ */
+SinglyLinkedList *InitSinglyLinkedList(void);
+
+/**
+ * Judge if the singly linked pHead is empty.
+ * @param pHead
+ * @return If return 1: The pHead is not empty. If return 0: The pHead is empty
+ */
+int IsEmpty(SinglyLinkedList *pHead);
+
+
+/**
+ * Insert a element at the end of the singly linked list
+ * @param pHead
+ * @param elem
+ */
+void InsertElemToEnd(SinglyLinkedList *pHead, ElemType elem);
+
+/**
+ * Insert a element at the head of the singly linked list
+ * @param pHead
+ * @param elem
+ */
+void InsertElemToHead(SinglyLinkedList *pHead, ElemType elem);
+
+/**
+ * Return the size of the singly linked list
+ * @param pHead
+ * @return
+ */
+int GetSizeOfList(SinglyLinkedList *pHead);
+
+/**
+ * Get an element based on a given element value
+ * @param pHead: Head pointer of the singly linked list
+ * @param elem: Object element
+ * @return
+ */
+SinglyLinkedList *LocateElem(SinglyLinkedList *pHead, ElemType elem);
+
+/**
+ * Traverse the singly linked list
+ * @param pHead
+ */
+void TraverseLinkedList(SinglyLinkedList *pHead);
--- LinkedList/Source/SinglyLinkedList.c
+++ LinkedList/Source/SinglyLinkedList.c
@@ -1,1 +1,134 @@
 #include "SinglyLinkedList.h"
+
+/**
+ * Initialize a singly linked list with a head node
+ * @return
+ */
+SinglyLinkedList *InitSinglyLinkedList(void)
+{
+    struct Node *pHead = NULL;
+    pHead = (struct Node *) malloc(sizeof(struct Node));
+    if (pHead)
+    {
+        pHead->data = 0;
+        pHead->next = NULL;
+    }
+
+    return pHead;
+}
+
+
+/**
+ * Judge if the singly linked pHead is empty.
+ * @param pHead
+ * @return If return 1: The pHead is empty. If return 0: The pHead is not empty
+ */
+int IsEmpty(SinglyLinkedList *pHead)
+{
+    return pHead->next == NULL;
+}
+
+
+/**
+ * Insert a element at the end of the singly linked list
+ * @param pHead
+ * @param elem
+ */
+void InsertElemToEnd(SinglyLinkedList *pHead, ElemType elem)
+{
+    // The pointer p point to the head node
+    struct Node *p = pHead;
+
+    // The pointer p finally point to the last node of the singly linked list
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+
+    struct Node *node = (struct Node *) malloc(sizeof(struct Node));
+    if (!node)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    node->data = elem;
+    node->next = NULL;
+    p->next = node;
+}
+
+
+/**
+ * Insert a element at the head of the singly linked list
+ * @param pHead
+ * @param elem
+ */
+void InsertElemToHead(SinglyLinkedList *pHead, ElemType elem)
+{
+    // The pointer p point to the head node
+    struct Node *p = pHead;
+
+    struct Node *node = (struct Node *) malloc(sizeof(struct Node));
+    if (!node)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    node->data = elem;
+    node->next = p->next;
+    p->next = node;
+}
+
+
+int GetSizeOfList(SinglyLinkedList *pHead)
+{
+
+    struct Node *p = pHead->next;
+
+    int total = 0;
+    while (p != NULL)
+    {
+        total++;
+        p = p->next;
+    }
+
+    return total;
+}
+
+
+/**
+ * Get an element based on a given element value
+ * @param pHead: Head pointer of the singly linked list
+ * @param elem: Object element
+ * @return
+ */
+SinglyLinkedList *LocateElem(SinglyLinkedList *pHead, ElemType elem)
+{
+    // Point to the node of the first element of the singly linked list
+    struct Node *p = pHead->next;
+
+    while (p != NULL && p->data != elem)
+    {
+        p = p->next;
+    }
+
+    return p;
+}
+
+
+/**
+ * Traverse the singly linked list
+ * @param pHead
+ */
+void TraverseLinkedList(SinglyLinkedList *pHead)
+{
+    struct Node *p = pHead->next;
+
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
