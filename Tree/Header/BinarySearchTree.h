57a32a5dba14c3b81e21b472a08bb62da0194e85
cxdeng <chengx.deng@icloud.com>
2022-12-20 00:37:32 +0000

    Traverse the binary search tree using breadth first

--- Tree/Header/BinarySearchTree.h
+++ Tree/Header/BinarySearchTree.h
@@ -16,6 +16,57 @@ struct BstNode
 };
 
 /**
+* Define node of singly linked list
+*/
+typedef struct QueueNode
+{
+    struct BstNode *data;
+    struct QueueNode *next;
+} BstLinkedQueue;
+
+extern struct QueueNode *bstFront;
+extern struct QueueNode *bstRear;
+
+/**
+ * Initialize a singly linked queue with a head node
+ * @return
+ */
+BstLinkedQueue *InitBstLinkedQueue();
+
+/**
+ * Push a element to the linked queue
+ * @param queue
+ * @param elem
+ */
+void BstEnqueue(BstLinkedQueue *queue, struct BstNode *elem);
+
+/**
+ * Pop a element from the linked queue
+ * @param queue
+ */
+void BstDequeue(BstLinkedQueue *queue);
+
+/**
+ * Pop a element from the linked queue
+ * @param queue
+ */
+void Dequeue(BstLinkedQueue *queue);
+
+/**
+ * Judge if the static circular queue is empty
+ * @param queue
+ * @return
+ */
+int IsEmpty(BstLinkedQueue *queue);
+
+/**
+ * Return the front element of the queue
+ * @param queue
+ * @return
+ */
+struct BstNode *Front(BstLinkedQueue *queue);
+
+/**
  * Create a new node of binary search tree
  * @param elem
  * @return
@@ -28,7 +79,7 @@ struct BstNode *GetNewNode(ElemType elem);
  * @param elem
  * @return
  */
-struct BstNode* InsertElem(struct BstNode *root, ElemType elem);
+struct BstNode *InsertElem(struct BstNode *root, ElemType elem);
 
 /**
  * Search an element from the binary search tree
@@ -72,3 +123,9 @@ ElemType FIndMaxRecursion(struct BstNode *root);
  * @return
  */
 int FindHeight(struct BstNode *root);
+
+/**
+ * Traverse the binary search tree using breadth first (level order)
+ * @param root
+ */
+void BreadthFirstTraverse(struct BstNode *root, struct BstNode *pNode);
--- Tree/Source/BinarySearchTree.c
+++ Tree/Source/BinarySearchTree.c
@@ -1,5 +1,120 @@
 #include "BinarySearchTree.h"
 
+extern struct QueueNode *bstFront = NULL;
+extern struct QueueNode *bstRear = NULL;
+
+/**
+ * Initialize a singly linked queue with a head node
+ * @return
+ */
+BstLinkedQueue *InitBstLinkedQueue()
+{
+    struct QueueNode *pHead = NULL;
+    pHead = (struct QueueNode *) malloc(sizeof(struct QueueNode));
+
+    if (pHead)
+    {
+        pHead->data = 0;
+        pHead->next = NULL;
+    }
+
+    return pHead;
+}
+
+
+/**
+ * Push a element to the linked queue
+ * @param queue
+ * @param elem
+ */
+void BstEnqueue(BstLinkedQueue *queue, struct BstNode *elem)
+{
+    // The pointer p point to the head node
+    struct QueueNode *p = queue;
+
+    struct QueueNode *node = (struct QueueNode *) malloc(sizeof(struct QueueNode));
+    if (!node)
+    {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    node->data = elem;
+    node->next = NULL;
+
+    // If the queue is empty, set the pointer 'front' and the pointer 'rear' point to the new node
+    if (bstFront == NULL && bstRear == NULL)
+    {
+        bstFront = node;
+        bstRear = node;
+        p->next = node;
+        return;
+    }
+
+    // If the queue is not empty, the last node points to the new node
+    bstRear->next = node;
+    // Then, the pointer 'rear' points to the new last node
+    bstRear = node;
+}
+
+
+/**
+ * Pop a element from the linked queue
+ * @param queue
+ */
+void BstDequeue(BstLinkedQueue *queue)
+{
+    // The pointer p point to the head node
+    struct QueueNode *p = queue;
+    struct QueueNode *delNode = bstFront;
+
+    if (bstFront == NULL)
+    {
+        printf("The queue is empty.");
+        return;
+    }
+    // The queue only has one element
+    if (bstFront == bstRear)
+    {
+        bstFront = NULL;
+        bstRear = NULL;
+    } else
+    {
+        bstFront = bstFront->next;
+    }
+    free(delNode);
+    p->next = bstFront;
+}
+
+
+/**
+ * Judge if the static circular queue is empty
+ * @param queue
+ * @return
+ */
+int IsEmpty(BstLinkedQueue *queue)
+{
+    if (bstRear == NULL)
+        return TRUE;
+    return FALSE;
+}
+
+
+/**
+ * Return the front element of the queue
+ * @param queue
+ * @return
+ */
+struct BstNode *Front(BstLinkedQueue *queue)
+{
+    if (IsEmpty(queue))
+    {
+        printf("The queue is empty.");
+        return NULL;
+    }
+
+    return bstFront->data;
+}
+
 
 /**
  * Create a new node of binary search tree
@@ -182,5 +297,35 @@ int FindHeight(struct BstNode *root)
     } else
     {
         return heightOfRightTree + 1;
+    }
+}
+
+
+/**
+ * Traverse the binary search tree using breadth first (level order)
+ * @param root
+ */
+void BreadthFirstTraverse(struct BstNode *root, struct BstNode *pNode)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    BstLinkedQueue *queue = InitBstLinkedQueue();
+    BstEnqueue(queue, root);
+
+    while (!IsEmpty(queue))
+    {
+        struct BstNode *p = Front(queue);
+        printf("%d ", p->data);
+        if (p->left != NULL)
+        {
+            BstEnqueue(queue, p->left);
+        }
+        if(p->right != NULL){
+            BstEnqueue(queue, p->right);
+        }
+        BstDequeue(queue);
     }
 }
